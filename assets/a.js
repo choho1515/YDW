944ab18a9d7fd1003e665444027f91e341a3e5d8
윤동원 <65482819+choho1515@users.noreply.github.com>
2022-12-18 18:11:53 +0900

    Create a.js

---
+++ assets/a.js
@@ -0,0 +1,35 @@
+//계산
+function calculate()
+{
+    let total=score.innerHTML;
+    let questionArray=document.getElementsByClassName("question");
+    let answerArray=document.getElementsByClassName("answer");
+
+    for(i=0; i<questionArray.length; i++)
+    {
+        let question = questionArray[i].innerHTML;
+        let correctanswer = eval(question);
+        let useranswer = answerArray[i].value;
+
+        console.log("parseInt(useranswer) : " + parseInt(useranswer));
+        console.log("correctanswer: " + correctanswer);
+           
+        //채점
+        if(useranswer=="" || isNaN(useranswer)) //아무것도 안적었을 때
+        {
+            total = total -5;
+            questionArray[i].style.textDecoration = "line-through";
+        }
+        else if(parseInt(useranswer)==correctanswer) //정답
+        {
+            total = total + 3;
+            questionArray[i].style.textDecoration = "none";
+        }
+        else //오답
+        {
+            total = total -5;
+            questionArray[i].style.textDecoration = "line-through";
+        }  
+    }
+    document.getElementById("score").innerHTML=total;
+}
