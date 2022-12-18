89faa4cd316058c65497ba414e095e9fb5e19121
윤동원 <65482819+choho1515@users.noreply.github.com>
2022-12-18 18:30:57 +0900

    Update a.js

--- assets/a.js
+++ assets/a.js
@@ -1,35 +1,25 @@
 //계산
-function calculate()
-{
-    let total=score.innerHTML;
-    let questionArray=document.getElementsByClassName("question");
-    let answerArray=document.getElementsByClassName("answer");
+function calculate() {
+  let total = parseInt(score.innerHTML); // total 변수를 정수로 저장
+  let questionArray = document.getElementsByClassName("question");
+  let answerArray = document.getElementsByClassName("answer");
 
-    for(i=0; i<questionArray.length; i++)
-    {
-        let question = questionArray[i].innerHTML;
-        let correctanswer = eval(question);
-        let useranswer = answerArray[i].value;
+  for (i = 0; i < questionArray.length; i++) {
+    let question = questionArray[i].innerHTML;
+    let correctanswer = eval(question);
+    let useranswer = answerArray[i].value;
 
-        console.log("parseInt(useranswer) : " + parseInt(useranswer));
-        console.log("correctanswer: " + correctanswer);
-           
-        //채점
-        if(useranswer=="" || isNaN(useranswer)) //아무것도 안적었을 때
-        {
-            total = total -5;
-            questionArray[i].style.textDecoration = "line-through";
-        }
-        else if(parseInt(useranswer)==correctanswer) //정답
-        {
-            total = total + 3;
-            questionArray[i].style.textDecoration = "none";
-        }
-        else //오답
-        {
-            total = total -5;
-            questionArray[i].style.textDecoration = "line-through";
-        }  
+    // 채점
+    if (useranswer === "") { // 아무것도 안적었을 때
+      total = total - 5;
+      questionArray[i].style.textDecoration = "line-through";
+    } else if (parseInt(useranswer) === correctanswer) { // 정답
+      total = total + 3;
+      questionArray[i].style.textDecoration = "none";
+    } else { // 오답
+      total = total - 5;
+      questionArray[i].style.textDecoration = "line-through";
     }
-    document.getElementById("score").innerHTML=total;
+  }
+  document.getElementById("score").innerHTML = total;
 }
